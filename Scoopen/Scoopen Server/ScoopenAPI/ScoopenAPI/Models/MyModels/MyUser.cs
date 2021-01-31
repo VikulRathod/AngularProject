2d6a52f37f662ddc076b189b0bddcfc9d76044e9
VikulRathod <vikul.rathod.net@gmail.com>
2021-01-31 08:42:45 +0530

    first commit

---
+++ Scoopen/Scoopen Server/ScoopenAPI/ScoopenAPI/Models/MyModels/MyUser.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ScoopenAPI.Models
+{
+    [MetadataType(typeof(UserMetadata))]
+    public partial class User
+    {
+
+    }
+
+    public class UserMetadata
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("isDeleting")]
+        public Nullable<bool> IsDeleted { get; set; }
+
+        [JsonProperty("role")]
+        public Nullable<int> RoleId { get; set; }
+    }
+}
---
+++ Scoopen/Scoopen Server/ScoopenAPI/ScoopenAPI/Models/MyModels/MyUserRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScoopenAPI.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    
+    public partial class UserRole
+    {
+    }
+}
