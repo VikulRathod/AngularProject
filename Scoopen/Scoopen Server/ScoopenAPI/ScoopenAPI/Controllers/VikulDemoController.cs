e068fb2a400a485bf0846e4265a85ef75e289b1e
vikul rathod <vikul.rathod.net@gmail.com>
2021-07-20 13:22:25 +0530

    implemented code from user story CIMS-6

---
+++ Scoopen/Scoopen Server/ScoopenAPI/ScoopenAPI/Controllers/VikulDemoController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ScoopenAPI.Controllers
+{
+    public class VikulDemoController : ApiController
+    {
+        // I will write code according to my user story according to my task
+    }
+}
